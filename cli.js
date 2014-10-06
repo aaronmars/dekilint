ea2d1734f28f43d9dd1152e15963b66523c28710
Aaron Mars <mars.aaron@gmail.com>
2014-10-06 16:13:40 -0700

    Workaround for missing env variables

--- cli.js
+++ cli.js
@@ -1,15 +1,26 @@
-var exec = require("child_process").execFile,
+var childProcess = require("child_process"),
     path = require("path");
+
 module.exports = {
     lint: function(options) {
-        var linterPath = options.mtcpExe || process.env.MINDTOUCH_MTCPEXE_PATH,
-            linterBin = path.join(linterPath, "mtcp.exe");
-        exec("mono", [
-            linterBin,
-            "dekiscriptlint",
-            "-f", options.dekiFile
-        ], {}, function(err, stdout) {
-            console.log(stdout);
+        var linterPath = null,
+            exportsCommand = process.env.SHELL + " -lc export";
+        childProcess.exec(exportsCommand, function(error, stdout) {
+            stdout.trim().split("\n").forEach(function(definition) {
+                var def = definition.split("=", 2);
+                if (def[0] === "MINDTOUCH_MTCPEXE_PATH") {
+                    linterPath = def[1];
+                }
+            });
+            console.error("Linter Path = " + linterPath);
+            var linterBin = path.join(linterPath, "mtcp.exe");
+            childProcess.execFile("mono", [
+                linterBin,
+                "dekiscriptlint",
+                "-f", options.dekiFile
+            ], {}, function(err, stdout) {
+                console.log(stdout);
+            });
         });
     }
 };
